5cf821eea8fa250391814f50a3ac6e0cd516d158
shintaro79 <shintaro.ishimine@aktsk.jp>
2021-04-15 19:39:39 +0900

    setup cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{App, AppSettings, SubCommand};
+
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let options = App::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("extract")
+                .about("Extract data as csv files")
+        )
+        .subcommand(
+            SubCommand::with_name("validate")
+                .about("Run your validations")
+        );
+
+    let matchers = options.get_matches();
+    match matchers.subcommand() {
+        ("extract", sub_match) => {
+            unimplemented!();
+        }
+        ("validate", sub_match) => {
+            unimplemented!();
+        }
+        _ => unreachable!(),
+    }
 }
